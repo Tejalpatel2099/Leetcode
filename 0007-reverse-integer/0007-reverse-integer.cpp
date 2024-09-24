19c0979aec7457be710592808d264add9006c417
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-09-23 17:33:25 -0700

    Time: 0 ms (100%), Space: 7.4 MB (100%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+       long  result = 0; 
+
+        while( x !=  0) {
+            int lastDigit = x % 10;
+            if ( result > INT_MAX/10) {
+                return 0; 
+            }else if (result < INT_MIN/10) {
+                return 0;
+            }
+            result = (result * 10) + lastDigit;
+            x = x / 10;
+        }
+        return result;
+    }     
+};
