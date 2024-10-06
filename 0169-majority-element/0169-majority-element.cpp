8a3c3804c22d3bfe92fc2404883b98c23dd38a8d
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-10-05 17:18:25 -0700

    Time: 1626 ms (5%), Space: 27 MB (79.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) { // brute force
-        int n = nums.size();
-        for (int val : nums) { // intefger value in nums // value removed
+    int majorityElement(std::vector<int>& nums) {
+        int n = nums.size(); // Get the size of the array
+        for (int i = 0; i < n;
+             ++i) { // outer loop to iterate through each element in array
+            int val = nums[i];
             int freq = 0;
 
-            for (int element : nums) {
-                if (element == val) {
-                    freq++;
+            for (int j = 0; j < n; ++j) { // inner loop
+                if (nums[j] == val) {     // if found value
+                    freq++;               // frequency increment
                 }
             }
-            if (freq > n / 2) {
+            if (freq > n / 2) { // if frequency is greater than n/2 then we
+                                // found majority element.
                 return val;
             }
         }
