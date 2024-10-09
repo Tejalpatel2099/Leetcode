f85fbf57092d8ab160f5a925e7a1f28b6fdf27ca
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-10-08 18:05:00 -0700

    Time: 77 ms (56.58%), Space: 70.5 MB (88.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,33 @@
+
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {  // brute force approach 
-        int maxSum = nums[0];
+    int maxSubArray(vector<int>& nums) { // brute force approach
+        //     int maxSum = nums[0];
+        //     int n = nums.size();
+
+        //     for (int start = 0; start < n; start++) {
+        //         int currentSum = 0;
+        //         for (int end = start; end < n; end++) {
+        //             currentSum = currentSum + nums[end];  // add end value in
+        //             current sum maxSum = max(maxSum, currentSum);
+        //         }
+        //     }
+
+        //     return maxSum;
+        // }
+
+        // kadanes algorithm
+        int currentSum = 0;
+        int maxSum = INT_MIN;
         int n = nums.size();
-        
-        for (int start = 0; start < n; start++) {
-            int currentSum = 0;
-            for (int end = start; end < n; end++) {
-                currentSum = currentSum + nums[end];  // add end value in current sum 
-                maxSum = max(maxSum, currentSum);
+
+        for(int i = 0; i < n; i++) {
+            currentSum = currentSum + nums[i];
+            maxSum = max(currentSum, maxSum);
+            if (currentSum < 0) {
+                currentSum = 0;
             }
         }
-        
         return maxSum;
     }
-
 };
