97056c485c9432f660c6e74c73eae8fe557d2c03
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-10-13 17:14:54 -0700

    Time: 5 ms (25.8%), Space: 10.2 MB (77.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());  // in built sort function O(log n)
+
+        int n = nums.size();
+        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else if (nums[i] == 1)
+                count1++;
+            else
+                count2++;
+        }
+        int index = 0;
+
+        for (int i = 0; i < count; i++) {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count1; i++) {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count2; i++) {
+            nums[index++] = 2;
+        }
     }
 };
