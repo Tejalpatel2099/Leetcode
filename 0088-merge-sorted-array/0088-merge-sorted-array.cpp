702ac87b084d24842db51a773be83ae4b8ba87b0
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-10-20 18:56:38 -0700

    Time: 0 ms (100%), Space: 11.3 MB (66.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0, i = m; j<n; j++){
-            nums1[i] = nums2[j];
-            i++;
-        }
-        sort(nums1.begin(),nums1.end());
-    }
+        int i = m - 1;
+        int j = n - 1;
+        int index = m + n - 1;
 
+
+// loop Merging from the End If the current element in nums1 (nums1[i]) is greater than or equal to the current element in nums2 (nums2[j]), place nums1[i] at position index in nums1.
+// Otherwise, place nums2[j] at position index in nums1.
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[index] = nums1[i];
+                i--;
+            } else {
+                nums1[index] = nums2[j];
+                j--;
+            }
+            index--;
+        }
+        //Copy Remaining Elements from nums2
+        
+        while (j >= 0) {
+            nums1[index] = nums2[j];
+            j--;
+            index--;
+        }
+    }
 };
