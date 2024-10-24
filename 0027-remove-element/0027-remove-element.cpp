7f9bded78ee22ce2657a8b1e37d92846930d066c
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-10-24 16:59:26 -0700

    Time: 0 ms (100%), Space: 10.7 MB (76.23%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
+        int j = 0;
+
         for (int i = 0; i < nums.size(); i++) {
+
+            // if curent element is not equal to val
+
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+
+                // move position to next valid element
+                nums[j] = nums[i];
+                
+                j++;
             }
         }
-        return k;
+        return j;
     }
+    
 };
