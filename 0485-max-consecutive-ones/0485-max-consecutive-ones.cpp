399c06b2d0f71513f592a802ac1f9517b127e934
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2024-11-04 17:22:22 -0800

    Time: 0 ms (100%), Space: 49.2 MB (24.35%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -8,7 +8,7 @@ public:
             if (nums[i] == 1) {
                 count++;  // Increment count for consecutive 1s
                 if (count > maxCount) {
-                    maxCount = count;  // Update maxCount if currentCount is greater
+                    maxCount = count;  // Update  maxCount if cureent count is greater , if not greater than reset it to zero. 
                 }
             } else {
                 count = 0;  // Reset currentCount when it is 0 
