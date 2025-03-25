f7b85e0606864deac80036540d65f6df4e0c985d
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-03-24 17:16:46 -0700

    Time: 44 ms (24.82%), Space: 14.1 MB (77.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,36 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         //  BRUTE FORCE APPROACH
-
-        //     for (int i = 0; i < nums.size(); i++) {
-        //         for (int j = i + 1; j < nums.size(); j++) {
-
-        //             if (nums[i] + nums [j] == target) {
-        //                 return {i, j};
-        //             }
-        //         }
-        //     }
-        //     return {};
-        // }
-
-        // ...........HASHING...........
-
-        unordered_map<int, int> m;
-        vector<int> result; // to store index
-
-        for (int i = 0; i < nums.size(); i++) {
-            int first = nums[i];
-            int second = target - first;
-
-            if (m.find(second) != m.end()) {  // search for second value in map 
-                result.push_back(i);
-                result.push_back(m[second]);
-
-                break;
+            for (int i = 0; i < nums.size(); i++) {
+                for (int j = i + 1; j < nums.size(); j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return{i,j};
+                    }
+                }
             }
-
-            m[first] = i; // if not break then
-        }
-        return result;
+            return {};
     }
 };
