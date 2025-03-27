3ec3f6e9010fa9407ae61198250102982c9e2b0a
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-03-27 16:54:48 -0700

    Time: 0 ms (100%), Space: 13.5 MB (97.82%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int binarySearch(vector<int>&nums,int start,int end,int target){
-        while(start<=end){
-            int mid = start + (end-start)/2;
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
 
-            if(nums[mid]<target){
-                start = mid+1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-            else if(nums[mid]>=target){
-                end = mid-1;
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else { 
+                right = mid - 1;
             }
         }
-        return start;
-    }
-
-
-    int searchInsert(vector<int>& nums, int target) {
-        
-        return binarySearch(nums,0,nums.size()-1,target);
-
+        return left;
     }
 };
