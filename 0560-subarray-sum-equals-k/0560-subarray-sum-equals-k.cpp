782a65acf92e02ea418202467b81469be66caf06
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-04-10 10:52:23 -0700

    Time: 35 ms (90.12%), Space: 46.8 MB (40.08%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,14 +4,29 @@ public:
         int n = nums.size();
         int count = 0;
 
-        for (int i =0 ; i < n; i++) {
-            int sum = 0;
+        vector<int>prefixSum(n, 0);
+        prefixSum[0]= nums[0];
 
-            for (int j = i; j < n; j++) {
-                sum = sum + nums[j];
-                if (sum == k) 
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i]; // to claculate prefix sum 
+        }
+
+        // to claculate count 
+        unordered_map<int, int> m;
+        for (int j = 0; j < n; j++) {
+            if (prefixSum[j] == k) {
                 count++;
             }
+            int val = prefixSum[j] - k;
+            if (m.find(val) != m.end()) {
+                count = count + m[val];
+            }
+            // if ps is alraedy in map then do freq++
+
+            if (m.find(prefixSum[j]) == m.end()) {
+                m[prefixSum[j]] = 0;
+            }
+            m[prefixSum[j]]++;
         }
         return count;
     }
