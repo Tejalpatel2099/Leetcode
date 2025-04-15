bbbf335429e4068fe4b7c27efa621d2d8efaf03d
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-04-15 13:18:54 -0700

    Time: 45 ms (24.82%), Space: 64.1 MB (29.3%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,17 @@
+// Brute force
+// Create a set from the array (nums).
+// Loop from 1 to n, and if a number is not in the set, add it to the result.
+//
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_set<int> seen(nums.begin(), nums.end());
+        vector<int> result;
+        for (int i = 1; i <= nums.size(); ++i) {
+            if (seen.find(i) == seen.end())
+                result.push_back(i);
+        }
+        return result;
+    }
+};
