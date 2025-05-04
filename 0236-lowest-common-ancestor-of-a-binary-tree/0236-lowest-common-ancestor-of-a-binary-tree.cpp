1bf678dfdb0112c87a063851258008dc3d89b168
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-04 15:18:32 -0700

    Time: 12 ms (63.97%), Space: 17.5 MB (40.87%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,15 +13,28 @@ public:
         if (root == nullptr) {
             return root;
         }
+
+        //f the current node matches either p or q, we return it.
+//Why? Because a node is its own ancestor.
         int curr = root->val;
 
         if(curr == p->val || curr == q->val) return root;
-
+//Recurse into the left and right subtrees to find p and q.
         TreeNode* left = lowestCommonAncestor(root->left,p,q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
+ //If:
+
+//p is found in one subtree, and
+//q is found in the other subtree,
+//Then the current node (root) is their lowest common ancestor. ✅
+
+
         if(left != NULL && right != NULL) return root;
 
+
+// If only one side (left or right) returned non-null, we pass it up.
+//This means both p and q are in the same subtree, or one hasn't been found yet.
         if (left == nullptr) {
             return right;
         }
