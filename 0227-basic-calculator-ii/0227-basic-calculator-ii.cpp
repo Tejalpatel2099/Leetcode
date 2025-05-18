3bdab5da98599ad50e939d467c61435827955802
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-17 20:00:04 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int i = 0, ans = 0, num = 0;
+        int interim = 0;
+        char op = '+';
+        while(i < s.size()){
+            if(isdigit(s[i])){
+                num = 0;
+                while(i < s.size() && isdigit(s[i])){
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                if(op == '+' || op == '-'){
+                    ans += interim;
+                    interim = num * (op == '-' ? -1 : 1);
+                }else if(op == '*'){
+                    interim *= num;
+                }else if(op == '/'){
+                    interim /= num;
+                }
+                continue;
+            }
+            else if(s[i] != ' ') op = s[i];
+           
+            i++;
+        }    
+
+        ans += interim;            
+        return ans;
+    }
+};
