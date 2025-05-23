65f85998e2bd8a696c1b8adfd420c6dd61e3649a
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-23 16:18:39 -0700

    Time: 0 ms (100%), Space: 42.3 MB (9.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -37,3 +37,21 @@ public:
     }
     
 };
+
+// ....brute force .............
+// // class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         vector<int> result(nums.size());
+//         for(int i=0; i < nums.size(); i++){
+//             int product = 1;
+//             for(int j=0; j < nums.size(); j++){
+//                 if(i != j){
+//                     product = product * nums[j];
+//                 }
+//             }
+//             result[i] = product;
+//         }
+//         return result;
+//     }
+//
