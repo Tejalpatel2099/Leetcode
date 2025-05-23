32a091a23814bcd17c1ab99a06e1e419342c010a
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-23 14:04:04 -0700

    Time: 55 ms (31.45%), Space: 90.8 MB (39.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s;
+        unordered_set<int> s;
 
         for ( int i = 0; i < nums.size(); i++) {
           if (s.find(nums[i]) != s.end()) {
-            return true; 
+            return true; // duplicate found
           }
            s.insert(nums[i]);
         }
-        return false;
+        return false; // no duplicate found 
     }
 };
+
+// ........Brute force approach 
+// bool containsDuplicate(vector<int>& nums) {
+// for ( int i = 0; i < nums.size(); i++) {
+    // for (int j = i + 1; j < nums.size(); j++) {
+        // if(nums[i] == nums[j]) 
+        //return true;
+    //}
+//}
+//return false;
+// tc  :- o(n2) sc :- O(1)
