77535f0d59e7dd9bce5280593fda2e4c3a751446
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-23 13:37:16 -0700

    Time: 2 ms (32.26%), Space: 97.2 MB (87.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,22 @@
+// You're given a list of stock prices where prices[i] is the price of a stock
+// on day i.
+// You want to buy one day and sell on a later day to get the maximum profit.
+// Return the maximum profit you can achieve. If you can't make a profit, return
+// 0.
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    int minPrice = INT_MAX;  // Start with the highest possible number
-    int maxProfit = 0; 
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
         for (int i = 0; i < prices.size(); i++) {
-        if (prices[i] < minPrice)
-            minPrice = prices[i];  // Found a lower price → better day to buy
-        else if (prices[i] - minPrice > maxProfit)
-            maxProfit = prices[i] - minPrice;  // Found a better profit → update max 
-    }
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
         return maxProfit;
     }
 };
