d0f330f6af2b0ab9b693d0a7038fc3be0ad5dac9
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-23 16:11:07 -0700

    Time: 0 ms (100%), Space: 41.9 MB (33.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,3 +1,16 @@
+
+//  Brute Force Idea (Optional – shows thought process)
+// "My first instinct is a brute force approach where for each element at index i, I compute the product of all elements except nums[i]. That would involve two nested loops, giving a time complexity of O(n²), which is inefficient for large arrays."
+
+// \U0001f539 2. Optimized Approach – Prefix and Suffix Products
+// _"To optimize this, I’ll use two auxiliary arrays: prefix and suffix.
+
+// prefix[i] will store the product of all elements before index i.
+
+// suffix[i] will store the product of all elements after index i.
+// Finally, the result at index i will be prefix[i] * suffix[i]."_
+//tc :- O(n) and SC:- O(n)  
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
