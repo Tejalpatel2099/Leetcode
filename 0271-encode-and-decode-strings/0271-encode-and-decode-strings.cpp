8bfb472c28898e6e57c1e46d0bf4ffd488403e8d
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-29 22:42:05 -0700

    Time: 34 ms (40.93%), Space: 29.3 MB (16.5%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
@@ -1,34 +1,38 @@
 class Codec {
 public:
-   // Encodes a list of strings to a single string.                                                                                             
-   std::string encode(std::vector<std::string>& strs) {
-     std::ostringstream oss;
-     for (int i = 0; i < static_cast<int>(strs.size()); ++i) {
-       oss << std::setfill('0') << std::setw(20) << strs[i].size();
-       oss << strs[i];
-     }
-   return oss.str();
- }
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string encoded = "";
 
- // Decodes a single string to a list of strings.                                                                                             
- std::vector<std::string> decode(std::string s) {
-   int i = 0;
-   std::vector<std::string> strs;
-   while (i <= static_cast<int>(s.size()) - 20) {
-     std::istringstream iss(s.substr(i, 20));
-     int str_len;
-     iss >> str_len;
-     i += 20;
-     if (str_len > 0)
-       strs.push_back(s.substr(i, str_len));
-     else
-       strs.push_back(std::string(""));                                                                                                       
-     i += str_len;
-  }
-  return strs;
-  }
+        for (string s : strs) {
+            // Store length of string followed by '#' and then the string itself
+            encoded += to_string(s.size()) + "#" + s;
+        }
+
+        return encoded;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+
+            // Find the position of the '#' delimiter to get the length
+            while (s[j] != '#') {
+                j++;
+            }
+
+            int len = stoi(s.substr(i, j - i)); // Extract the length of the string
+            string word = s.substr(j + 1, len); // Extract the word of 'len' characters
+
+            result.push_back(word);
+
+            i = j + 1 + len; // Move to the start of the next encoded word
+        }
+
+        return result;
+    }
 };
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec;
-// codec.decode(codec.encode(strs));
