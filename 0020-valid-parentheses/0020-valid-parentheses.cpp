acde707a26948e47c3215d60e73438da297cb7b3
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-29 21:44:34 -0700

    Time: 0 ms (100%), Space: 8.9 MB (21.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,3 +1,14 @@
+// I’m given a string s that contains only '(', ')', '{', '}', '[', and ']'.I need to return true if the brackets are balanced — that means:Every opening bracket has a matching closing bracket of the same type Brackets must close in the correct order
+// Questions :- 
+// Can I assume: The string contains only bracket characters?
+// The input can be empty — and if it is, should I return true?
+
+//........Stack ......
+// here i can think of is using a stack to track open brackets so basically I scan the string left to right and if i see opening bracket , i will push the code onto stack and if i see closing bracket i check if the stac k is empty -> return false and if the top of stack is matchin opening bracket -> pop it a nd if it is mismatch -> return fasle At the end, the stack should be empty for the string to be valid.
+//Time Complexity: O(n) We loop through all n characters in the string exactly once.
+
+// Space Complexity: O(n) the stack stores all opening brackets
+
 class Solution {
 public:
     bool isValid(string str) {
