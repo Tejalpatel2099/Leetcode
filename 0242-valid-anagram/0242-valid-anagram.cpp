07946866d4a22c7e975877959e84ce4568f42f75
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-05-29 21:16:47 -0700

    Time: 0 ms (100%), Space: 9.9 MB (11.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,3 +1,13 @@
+// we are given two strings s and t and needs to return true if it is an anagram of s and if not return false so , valid anagram means that both strings must havce same lengt5h andthey must contain same characters with same frequency. 
+// 1. input strings are lowercase only ? 
+// so here i can think of is a brute force solution to sort both strings and compare them i mean sort s and t and then if they become equal after sorting they are anagarms so , TC:- O(n log n) for each sort → overall O(n log n)
+// and sc :-   O(1) if sorting in-place
+
+//.......optimized ...............
+// we can improve this to O(n) time using hashmap or array to count character frequenceies. 
+// if length differs return false and create a frequency count of each character in s , decrese the same count as we go through s , and if any count goes below zero -> not an anagram.  
+// Tc :- o(n) and sc :- o(1)
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
