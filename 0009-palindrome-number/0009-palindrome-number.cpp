3e61dbacf7304ecd48c38f3d879bf647201f04a5
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-06-01 17:29:53 -0700

    Time: 3 ms (46.97%), Space: 8.6 MB (68.87%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,18 @@
 // Can i assume that input is a 32-bit signed integer? Also, do we consider negative numbers as non-palindromes by definition? 
+// What should I return for single-digit numbers?
 ///The task is to check if the given number reads the same forwards and backwards. 
-//...............brute force ...........
-//One brute-force approach would be to convert the integer to a string, reverse it, and compare it to the original string. But that uses extra space for the string conversion
-//Time Complexity: O(n) where n is number of digits
-// Space Complexity: O(n) due to the string
 
-// ......optimized ...........
-//A more optimized solution is to reverse the number mathematically and compare it with the original. If both are equal, it's a palindrome.
+//..............Brute force .....
+// First, I considered the brute-force approach where I convert the number to a string, reverse it, and compare it with the original. This works, but it uses O(n) time and O(n) space, where n is the number of digits.
+// ..........Optimized .............
+// So to make to make the time complexity efficient I can think of is a optimized approach where I will be reversing the number mathematically and compare it with original and if both are equal it means that it's a palindrome.
+
+// One thing I’m careful about is handling negative numbers — those are never palindromes by definition, so I return false immediately.
+
+// I also used a long type for the reversed number to guard against integer overflow, even though it may not always be necessary if we're given a 32-bit int.”.
+
+//The time complexity is O(log x) because I divide the number by 10 in each iteration, reducing the size logarithmically.
+// Space Complexity: O(1) — only a few variables, no additional data structures used.
 
 
 class Solution {
@@ -30,6 +36,4 @@ public:
     }
 };
 
-// TC:- O(log n) 
-// Sc  :- O(1) 
 
