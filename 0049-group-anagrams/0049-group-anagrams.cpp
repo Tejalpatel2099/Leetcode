fc1c7e6c8363014655764e812cd748fe7d9c3ea3
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-01 14:35:26 -0700

    Time: 11 ms (91.18%), Space: 25.1 MB (65.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,42 @@
+// we are given an array of strings and need to group the strings that are anagrams of each other. and i need to return list of groups where each group conatins strings that are anagrams . 
+
+// questions :- 
+// 1. Can i assume all strings are lowercase ?
+//2 . does the order of groups matter in outptut ? 
+
+
+//  ... using hash map ......q1 
+//  so anagrasm have teh same characters in different order so, if we sort the letters of any two anagrams, the result willbe same string . 
+// for eachs tring , sort it , use that sorted string as a key in hash map and store the original(unsorted string ) in value and all anagrams willhave the same sorted key , so they will be grouped together under that key. finally collect allvalues from hash map and retunthem . 
+// tc:-  You sort it, which takes O(k log k)
+// You do this for n strings → O(n × k log k) and sc :- O(n × k)
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // This hash map will store groups of anagrams
+        // Key: sorted version of the string
+        // Value: list of original strings that are anagrams of that key
         unordered_map<string, vector<string>> mp;
-        vector<vector<string>> anagrams;
 
-        // Populate the hash map with sorted keys
+        // Loop through each string in the input list
         for (int i = 0; i < strs.size(); i++) {
-            string sortedString = strs[i];
-            sort(sortedString.begin(), sortedString.end());
+            string sortedString = strs[i]; // Copy the original string
+            sort(sortedString.begin(), sortedString.end()); // Sort the characters alphabetically
+
+            // Use the sorted string as a key and add the original string to its group
             mp[sortedString].push_back(strs[i]);
         }
 
-        // Extract values from the hash map into the output vector
-        auto it = mp.begin();
-        while (it != mp.end()) {
-            anagrams.push_back(it->second);
-            ++it;
+        // Now we need to collect all the grouped anagrams from the map
+        vector<vector<string>> anagrams; // This will store the final result
+
+        // Loop through the map
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            anagrams.push_back(it->second); // Add each group (which is a vector of strings) to the result
         }
 
-        return anagrams;
+        return anagrams; // Return all grouped anagrams
     }
 };
