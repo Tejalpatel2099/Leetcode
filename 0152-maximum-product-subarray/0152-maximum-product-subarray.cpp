3963b9845b6d5692cef0fe372837b5cc6cb15bd9
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-04 12:30:07 -0700

    Time: 0 ms (100%), Space: 17.9 MB (5.75%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,3 +1,8 @@
+// I use a prefix–suffix product approach.
+// I multiply elements from both ends, resetting whenever I hit a zero, and keep track of the maximum product seen.
+// This single pass automatically handles positives, negatives, and zeros—giving the maximum product subarray efficiently in O(n) time
+
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
@@ -17,3 +22,25 @@ public:
         return ans;
     }
 };
+
+// Case 1:
+// If all numbers are positive, multiplying them all gives the maximum product.
+// → Entire array is the answer.
+
+// Case 2:
+// If there are even negatives, their product becomes positive.
+// → Again, entire array gives the max product.
+
+// Case 3:
+// If there are odd negatives, one of them must be removed (since removing one negative makes the count even → product positive).
+// → The max product will be from either:
+
+// the subarray before the first negative, or
+
+// the subarray after the last negative.
+
+// Case 4:
+// If there are zeros, they act as separators (since any product including 0 becomes 0).
+// → Split the array at 0s, solve for each non-zero part using Case 3 logic, and take the overall max.
+
+
