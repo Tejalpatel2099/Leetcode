800c8d5b71d9832f6555cd8e6612ddf9f927c5a8
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-05 12:15:40 -0700

    Time: 6 ms (19.73%), Space: 17.9 MB (59.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,34 +1,59 @@
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freqMap;
+        priority_queue<pair<int, int>> pq;
+        unordered_map<int, int> mpp;
 
-        // Step 1: Count frequency of each number
-        for (int num : nums) {
-            freqMap[num]++;
+        for (auto i : nums) {
+            mpp[i]++;
         }
 
-        // Step 2: Create buckets. Index = frequency, Value = list of numbers
-        int n = nums.size();
-        vector<vector<int>> buckets(n + 1); // because max frequency can be n
-
-        for (auto& pair : freqMap) {
-            int number = pair.first;
-            int frequency = pair.second;
-            buckets[frequency].push_back(number);
+        for (auto i : mpp) {
+            int key = i.first;
+            int val = i.second;
+            pq.push(make_pair(val, key));
         }
 
-        // Step 3: Collect k most frequent elements from the end
-        vector<int> result;
-        for (int i = n; i >= 0 && result.size() < k; --i) {
-            for (int num : buckets[i]) {
-                result.push_back(num);
-                if (result.size() == k) {
-                    break;
-                }
+        vector<int> ans;
+        while (k > 0) {
+            if (!pq.empty()) {
+                pair<int, int> i = pq.top();
+                ans.push_back(i.second);
+                pq.pop();
             }
+            k--;
         }
-
-        return result;
+        return ans;
     }
 };
+
+// unordered_map<int, int> freqMap;
+
+// // Step 1: Count frequency of each number
+// for (int num : nums) {
+//     freqMap[num]++;
+// }
+
+// // Step 2: Create buckets. Index = frequency, Value = list of numbers
+// int n = nums.size();
+// vector<vector<int>> buckets(n + 1); // because max frequency can be n
+
+// for (auto& pair : freqMap) {
+//     int number = pair.first;
+//     int frequency = pair.second;
+//     buckets[frequency].push_back(number);
+// }
+
+// // Step 3: Collect k most frequent elements from the end
+// vector<int> result;
+// for (int i = n; i >= 0 && result.size() < k; --i) {
+//     for (int num : buckets[i]) {
+//         result.push_back(num);
+//         if (result.size() == k) {
+//             break;
+//         }
+//     }
+// }
+
+// return result;
