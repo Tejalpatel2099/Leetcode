22c21cc37d00af3f92629d07641acfeca51cd813
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-10 15:29:56 -0700

    Time: 0 ms (100%), Space: 12.1 MB (17.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -10,11 +10,10 @@ public:
     // compare prefix with every other word
     for (int i = 1; i < strs.size(); i++) {
         while (strs[i].find(prefix) != 0) {
-            prefix = prefix.substr(0, prefix.size()- 1);
-
+            prefix = prefix.substr(0, prefix.size() - 1);
             if (prefix.empty()) return "";
         }
     }
     return prefix;
-    }
+    } 
 };  
