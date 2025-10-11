eef25fae05709006d88d6d5a1031d9d0b1eb5b8f
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-11 12:14:00 -0700

    Time: 0 ms (100%), Space: 8.9 MB (33.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,7 +5,7 @@
 
 //........Stack ......
 // here i can think of is using a stack to track open brackets so basically I scan the string left to right and if i see opening bracket , i will push the code onto stack and if i see closing bracket i check if the stack is empty -> return false and if the top of stack is matchin opening bracket -> pop it a nd if it is mismatch -> return fasle At the end, the stack should be empty for the string to be valid.
-//Time Complexity: O(n) We loop through all n characters in the string exactly once.
+//Time Complexity: O(n) We loop through all n characters in the string exactly once. and push and pop opeartion on stack is O(1). so total O(n)and n is  length of input string.
 
 // Space Complexity: O(n) the stack stores all opening brackets
 
