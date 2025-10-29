7f4ce56d03778f82a2ea3a52053274ac92c514fd
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-10-29 14:44:50 -0700

    Time: 0 ms (100%), Space: 9.8 MB (25.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,4 +1,4 @@
-// we are given two strings s and t and needs to return true if it is an anagram of s and if not return false so , valid anagram means that both strings must havce same lengt5h andthey must contain same characters with same frequency. 
+// we are given two strings s and t and needs to return true if it is an anagram of s and if not return false so , valid anagram means that both strings must havce same lengt5h and they must contain same characters with same frequency. 
 // 1. input strings are lowercase only ? 
 // so here i can think of is a brute force solution to sort both strings and compare them i mean sort s and t and then if they become equal after sorting they are anagarms so , TC:- O(n log n) for each sort → overall O(n log n)
 // and sc :-   O(1) if sorting in-place
