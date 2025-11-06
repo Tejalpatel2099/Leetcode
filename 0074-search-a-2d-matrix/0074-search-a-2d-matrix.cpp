1c7eb08d922e26097a91a3acc2e41e90468a1226
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-11-06 12:05:06 -0800

    Time: 0 ms (100%), Space: 13.3 MB (75.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();           // number of rows
-        int m = matrix[0].size();        // number of columns
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-        int low = 0, high = n * m - 1;   // virtual 1D array range
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;  // safer mid calculation
-            int row = mid / m;                 // map to row index
-            int col = mid % m;                 // map to column index
-
-            if (matrix[row][col] == target)
-                return true;
-            else if (matrix[row][col] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
         }
         return false;
     }
 };
-
-
-//................BRUTE FORCE ..........................//
-//     for (int i = 0; i < row; i++) {
-//         for (int j = 0; j < col; j++) {
-//             if (matrix[i][j] == target) {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// }
