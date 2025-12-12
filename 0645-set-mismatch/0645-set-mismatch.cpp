c97bc7b74d094836185f71469f0b828a5b9daa62
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-12-12 15:11:28 -0800

    Time: 0 ms (100%), Space: 26.3 MB (43.49%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -4,18 +4,19 @@ public:
     int n = nums.size();
 
         // count[i] will store how many times number i appears
-        vector<int> count(n + 1, 0);
+        // this creates an array count of size n+1 
+        vector<int> count(n + 1, 0); 
 
-        // STEP 1: count each number
+        //  count each number
         for (int i = 0; i < nums.size(); i++) {
             int x = nums[i];   // current number
             count[x]++;        // increase its count
         }
-
+// variables to store answers here , -1 is not found yet 
         int duplicate = -1;
         int missing = -1;
 
-        // STEP 2: find duplicate and missing number
+        //  find duplicate and missing number
         for (int i = 1; i <= n; i++) {
             if (count[i] == 2) {
                 duplicate = i;   // number appears twice
@@ -25,7 +26,7 @@ public:
             }
         }
 
-        // STEP 3: return result
+        //  return result
         return {duplicate, missing};
     }
 };
