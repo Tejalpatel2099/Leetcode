1dc81908d64733eb77e767f5357a9bd84a445755
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-12-29 15:09:32 -0800

    Time: 0 ms (100%), Space: 11.7 MB (46.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,18 +4,11 @@ public:
         int j = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-
-            // if curent element is not equal to val
-
             if (nums[i] != val) {
-
-                // move position to next valid element
-                nums[j] = nums[i];
-                
+                nums[j] = nums[i]; 
                 j++;
             }
         }
         return j;
     }
-    
 };
