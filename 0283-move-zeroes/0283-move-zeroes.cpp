b034a4bd35785e9e7ab65d69208e2733d75488bf
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2025-12-30 11:04:10 -0800

    Time: 0 ms (100%), Space: 23.8 MB (54.19%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
       int j = -1;
-
-        // Find the first zero
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 0) {
                 j = i;
@@ -11,16 +9,11 @@ public:
             }
         }
 
-        // If no zero found, return
         if (j == -1) return;
-
-        // Start from the next index of first zero
         for (int i = j + 1; i < nums.size(); i++) {
-            // If current element is non-zero
+           
             if (nums[i] != 0) {
-                // Swap with nums[j]
                 swap(nums[i], nums[j]);
-                // Move j to next zero
                 j++;
             }
         }
