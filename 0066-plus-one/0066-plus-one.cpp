6fdd00019e672524ed8d30ff51ce070f13bedd57
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-01-02 12:41:30 -0800

    Time: 0 ms (100%), Space: 11.4 MB (75.55%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
 
-        // start fropm last and move to left
-
+        // Traverse from last digit
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
+                digits[i]++;     // just add 1
+                return digits;   // done
             }
-            digits[i] = 0; // if the didgit is 9 we set it to 0 and carryu 1 to
-                           // next left didgit
+            digits[i] = 0;       // if digit was 9
         }
-        digits.insert(digits.begin(),1); // if all digits are 9 then add 1 at start
+
+        // If all digits were 9
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
