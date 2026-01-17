447fbac021c100730917e2f2020f531352c0651c
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-01-16 18:18:30 -0800

    Time: 0 ms (100%), Space: 13.3 MB (76.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,6 +4,8 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
+        
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == target) {
