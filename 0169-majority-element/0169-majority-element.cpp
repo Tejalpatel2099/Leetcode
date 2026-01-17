c3561109f9b9a3c30eab3009ea0ce558e1411db7
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-01-16 17:47:12 -0800

    Time: 6 ms (11.87%), Space: 28.3 MB (7.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,17 +4,16 @@ public:
         unordered_map<int, int> m;
         int n = nums.size();
 
-        // Step 1: Count occurrences of each element
         for (int i = 0; i < n; i++) {
             m[nums[i]]++;
 
-            // Step 2: Check if the current element is the majority element
+           
             if (m[nums[i]] > n / 2) {
 
-                return nums[i];  // Return the majority element
+                return nums[i];  
             }
         }
 
-        return -1;  
+        return -1;
     }
 };
