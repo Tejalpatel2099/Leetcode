a34ff9ac11759925b5466616e0cdaff247fa4dc5
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-02-06 10:11:29 -0800

    Time: 16 ms (14.41%), Space: 14.7 MB (38.59%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,37 +2,29 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        // Initialize variables
-        int openParenthesesCount = 0;
-        vector<char> arr(s.begin(), s.end()); // Convert string to character array
+        unordered_set<int>BI;
+        stack<int>st;
 
-        // First pass: mark excess closing parentheses with '*'
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == '(')
-                openParenthesesCount++;
-            else if (arr[i] == ')') {
-                if (openParenthesesCount == 0)
-                    arr[i] = '*'; // Mark excess closing parentheses
-                else
-                    openParenthesesCount--;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            }
+            else if (s[i] == ')') {
+                if (st.empty())BI.insert(i);
+                else st.pop();
+
             }
         }
-
-        // Second pass: mark excess opening parentheses from the end
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (openParenthesesCount > 0 && arr[i] == '(') {
-                arr[i] = '*'; // Mark excess opening parentheses
-                openParenthesesCount--;
+        while (!st.empty()) {
+            BI.insert(st.top());
+            st.pop();
+        }
+        string ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (!BI.contains(i)) {
+                ans.push_back(s[i]);
             }
         }
-        
-        // Filter out marked characters and store the result in a new string
-        string result = "";
-        for (char c : arr) {
-            if (c != '*')
-                result += c;
-        }
-
-        return result;
+        return ans;
     }
 };
