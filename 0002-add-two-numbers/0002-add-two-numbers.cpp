282e5935634d5d5190f69a3e4f6b244dd0931b0f
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-02-13 11:00:54 -0800

    Time: 3 ms (34.95%), Space: 77.2 MB (44.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -17,33 +17,36 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy = new ListNode(); 
-        ListNode *temp = dummy; 
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* curr = dummyHead;
+
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
         int carry = 0;
-        while( (l1 != NULL || l2 != NULL) || carry) {
-            int sum = 0; 
-            if(l1 != NULL) {    // l1 is not NULL (still has digits left).
-         
-                sum = sum + l1->val;   // If l1 is not NULL then add its value to  sum and move to next node in l1 
 
-                l1 = l1 -> next; 
+        while (temp1 != nullptr || temp2 != nullptr) {
+            int sum = carry;
+
+            if (temp1) {
+                sum += temp1->val;
+                temp1 = temp1->next;
             }
-            
-            if(l2 != NULL) {      //  l2 is not NULL (still has digits left).
-
-                sum = sum + l2 -> val; // if l2is not Null then add its value to sum and move to next node in l2 
-
-                l2 = l2 -> next; 
+            if (temp2) {
+                sum += temp2->val;
+                temp2 = temp2->next;
             }
-            
-            sum = sum + carry; //Add the previous carry value to sum. and update carry for next iteration 
 
-            carry = sum / 10; 
+            carry = sum / 10;
 
-            ListNode *node = new ListNode(sum % 10);  // craeting new node and attach to result 
-            temp -> next = node; 
-            temp = temp -> next; 
+            ListNode* newNode = new ListNode(sum % 10);
+            curr->next = newNode;
+            curr = curr->next;
         }
-        return dummy -> next;  // dummy-> next is head of LL  
+
+        if (carry) {
+            curr->next = new ListNode(carry);
+        }
+
+        return dummyHead->next; 
     }
 };
