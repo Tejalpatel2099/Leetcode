abf1ec47e9e138df6fde9862c5eed6c649cf2c99
Tejal Patel <70639514+Tejalpatel2099@users.noreply.github.com>
2026-05-18 10:19:05 -0700

    Time: 54 ms (50.45%), Space: 137.1 MB (48.44%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,13 +1,17 @@
+// I will store each key with alist of timestamp value pairs  
+
 class TimeMap {
 public:
     unordered_map<string, vector<pair<int, string>>> mp;
 
     TimeMap() {}
 
+// when set is called i will add timestamp and value to that keys list 
     void set(string key, string value, int timestamp) {
         mp[key].push_back({timestamp, value});
     }
 
+// when get is called i first check if key exist or not if not then return empty string and if key key exits i will do binary search through that. key timestamop list to find latest timestamp that is less athn or equal to given timestamp. 
     string get(string key, int timestamp) {
         if (mp.find(key) == mp.end()) {
             return "";
@@ -33,6 +37,10 @@ public:
         return ans;
     }
 };
+
+// set - 0(1) 
+// get - O(log n) 
+// space will be O(total number of set calls)
 
 /**
  * Your TimeMap object will be instantiated and called as such:
